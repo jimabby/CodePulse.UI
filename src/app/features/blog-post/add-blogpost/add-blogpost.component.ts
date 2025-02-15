41d12c46eac37c8701226d6c79328961dfe8f251
jim <wksunshine@gmail.com>
2025-02-15 16:19:23 +1000

    Add BlogPost

---
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+  model: AddBlogPost;
+
+  constructor(private blogPostService: BlogPostService, private router: Router) {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date(),
+    }
+  }
+
+  onFormSubmit(): void {
+    console.log("Submitting Payload: ", this.model)
+    this.blogPostService.createBlogPost(this.model)
+      .subscribe({
+        next: (response) => {
+          console.log("success: ", response)
+          this.router.navigateByUrl('/admin/blogposts');
+        },
+        error: (error) => {
+          console.error("submission error: ", error.error)
+        }
+      });
+  }
+}
